d19069b008549fdec6c612b0e40d69df989896da
vamshisuram <vamshi.suram25@gmail.com>
2014-09-05 11:47:05 +0530

    pipes added

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var mainState = {
     preload: function() {
         game.stage.backgroundColor = "#71c5cf";
         game.load.image('bird', 'assets/bird.png');
+        game.load.image('pipe', 'assets/pipe.png');
     },
 
     create: function() {
@@ -17,6 +18,11 @@ var mainState = {
 
         var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
+
+        this.pipes = game.add.group(); // Create a group
+        this.pipes.enableBody = true;  // Add physics to the group
+        this.pipes.createMultiple(20, 'pipe'); // Create 20 pipes
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
     },
 
     update: function() {
@@ -30,6 +36,24 @@ var mainState = {
 
     restartGame: function() {
         game.state.start('main');
+    },
+
+    addOnePipe: function(x, y) {
+        var pipe = this.pipes.getFirstDead();
+        pipe.reset(x, y);
+        pipe.body.velocity.x = -200;
+
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;
+    },
+
+    addRowOfPipes: function() {
+        var hole = Math.floor(Math.random() * 5) + 1;
+
+        for (var i= 0; i <8 ; i++) {
+            if(i != hole && i != hole + 1)
+                this.addOnePipe(400, i*60 + 10)
+        }
     }
 };
 
