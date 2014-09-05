c0c260fb87328a33187d37cee06fd0a2eafec951
vamshisuram <vamshi.suram25@gmail.com>
2014-09-05 11:48:31 +0530

    collision, score added

--- main.js
+++ main.js
@@ -23,11 +23,15 @@ var mainState = {
         this.pipes.enableBody = true;  // Add physics to the group
         this.pipes.createMultiple(20, 'pipe'); // Create 20 pipes
         this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0", {font: "30px Arial", fill: "#ffffff"});
     },
 
     update: function() {
         if(this.bird.inWorld == false)
             this.restartGame();
+        game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
     },
 
     jump: function() {
@@ -52,8 +56,11 @@ var mainState = {
 
         for (var i= 0; i <8 ; i++) {
             if(i != hole && i != hole + 1)
-                this.addOnePipe(400, i*60 + 10)
+                this.addOnePipe(400, i*60 + 10);
         }
+
+        this.score += 1;
+        this.labelScore.text = this.score;
     }
 };
 
